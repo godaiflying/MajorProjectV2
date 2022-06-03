6e42dc3b5e01a8115ffb76ee79bf2beee3ce2dcb
godaiflying <31681293+godaiflying@users.noreply.github.com>
2022-06-03 12:25:12 +1000

    Sever icomplete as well as html setup
    The socketio code was able to simpilfy all things to a minimum as well as the html being easy to set up

---
+++ server.py
@@ -0,0 +1,38 @@
+#pip install flasksocketio
+#https://blog.miguelgrinberg.com/post/learn-socket-io-with-python-and-javascript-in-90-minutes
+#https://medium.com/geekculture/making-your-own-chatroom-sockets-in-python-javascript-html-ac14c2870064
+#https://flask-socketio.readthedocs.io/en/latest/getting_started.html#initialization
+
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socket = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('client.html')
+
+@socket.on('connect')
+def connect():
+    print("[CLIENT CONNECTED]:", request.sid)
+
+
+@socket.on('disconnect')
+def disconn():
+    print("[CLIENT DISCONNECTED]:", request.sid)
+
+    
+@socket.on('notify')
+def notify(user):
+    emit('notify', user, broadcast=True, skip_sid=request.sid)
+
+
+@socket.on('data')
+def emitback(data):
+    emit('returndata', data, broadcast=True)
+
+
+if __name__ == "__main__":
+    socket.run(app)
+
